74975bbe513b8be60f756994121f267e547465e7
avvallack <avvallack@gmail.com>
2020-06-02 18:00:54 +0300

    basic function to retrieve data prepared

--- utilities.py
+++ utilities.py
@@ -5,10 +5,14 @@ import variables as vr
 
 def retrieve_data():
     """
-    retrieving task data
+    retrieving and basic preparation of data task data
     :return:
     pandas data frame with data object
     """
     col_names = [f'hist_{i}' for i in range(27)] + ['img_name', 'img_path', 'target']
-    return pd.read_csv(vr.DATA_PATH, compression='gzip', sep=' ', header=None, columns=col_names)
+    data = pd.read_csv(vr.DATA_PATH, compression='gzip', sep=' ', header=None, names=col_names)
+    data = data.drop(columns=['img_name', 'img_path'])
+    map_dic = {'Inlier': 0, 'Outlier': 1}
+    data.target.replace(map_dic, inplace=True)
+    return data
 
