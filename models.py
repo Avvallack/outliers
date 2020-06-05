927d9a76a7f3a963f520378ca05e66d786210884
avvallack <avvallack@gmail.com>
2020-06-05 15:22:29 +0300

    all supervised part was implemented

--- models.py
+++ models.py
@@ -1,13 +1,12 @@
+import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn import model_selection
 from sklearn import pipeline
-from sklearn.linear_model import LogisticRegressionCV
+from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import GridSearchCV
 from lightgbm import LGBMClassifier
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def basic_pipeline(classifier):
@@ -25,7 +24,7 @@ def supervised_estimation(grid, features, labels):
     """
     supervised estimation process
     estimating given grid and plotting roc curve and pr curve
-    :param grid: gridsearch sklearn object
+    :param grid: grid search sci-kit learn object
     :param features: pandas features to train model
     :param labels:  np array like labels
     :return: roc_auc, pr_auc, f1 metrics
@@ -41,20 +40,27 @@ def supervised_estimation(grid, features, labels):
     pr_auc = metrics.auc(recall, precision)
     f1 = metrics.f1_score(test_labels, grid.predict(test_features))
     roc_curve = metrics.roc_curve(test_labels, prediction)
-
+    # plot results
     fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(16, 6))
     ax[0].set_title('ROC Curve')
+    ax[0].plot([0, 1], [0, 1], linestyle='--', label='No Skill')
+    ax[0].plot(roc_curve[0], roc_curve[1], marker='.', label='ROC curve')
+    ax[0].set_xlabel('False Positive Rate')
+    ax[0].set_ylabel('True Positive Rate')
+    ax[0].legend()
     ax[1].set_title('PR Curve')
-    sns.lineplot(roc_curve[0], roc_curve[1], ax=ax[0])
-    sns.lineplot(recall, precision, ax=ax[1])
-
+    ax[1].plot([0, 1], [1, 0], linestyle='--', label='No Skill')
+    ax[1].plot(recall, precision, marker='.', label='PR curve')
+    ax[1].set_xlabel('Recall')
+    ax[1].set_ylabel('Precision')
+    ax[1].legend()
     return roc_auc, pr_auc, f1
 
 
 def knn():
     """
     KNN grid search implementation
-    :return: grid search sklearn object
+    :return: grid search sci-kit learn object
     """
     classifier = KNeighborsClassifier(n_jobs=-1)
     pipe = basic_pipeline(classifier)
@@ -72,51 +78,44 @@ def knn():
 def log_regression():
     """
     Log regression grid search implementation
-    :return: grid search sklearn object
+    :return: grid search sci-kit learn object
     """
-    classifier = LogisticRegressionCV(n_jobs=-1, max_iter=1e5, solver='saga', scoring='roc_auc')
+    classifier = LogisticRegression(n_jobs=-1, max_iter=1e5, solver='saga', penalty='elasticnet', l1_ratio=0.1)
     pipe = basic_pipeline(classifier)
-    param_grid = {'model_fitting__penalty': ['elasticnet'],
-                  'model_fitting__l1_ratio': [0, 0.2, 0.4, 0.6, 0.8, 1]}
+    param_grid = {'model_fitting__l1_ratio': [0.1, 0.2, 0.4, 0.6, 0.8, 0.9]}
     grid = GridSearchCV(pipe,
                         param_grid,
-                        scoring=['roc_auc'],
-                        cv=20,
-                        refit='roc_auc')
+                        scoring='roc_auc',
+                        cv=20)
+
     return grid
 
 
 def svm_classifier():
     """
     svm classifier grid search implementation
-    :return: grid search sklearn object
+    :return: grid search sci-kit learn object
     """
-    classifier = SVC(gamma='scale', probability=True, class_weight={0: 0.0001, 1: 1})
+    classifier = SVC(kernel='rbf', cache_size=2048, probability=True)
     pipe = basic_pipeline(classifier)
-    param_grid = {'model_fitting__C': [0.001, 0.003, 0.1, 0.3, 1, 3, 10, 30]}
-    grid = GridSearchCV(pipe,
-                        param_grid,
-                        scoring=['roc_auc'],
-                        cv=20,
-                        refit='roc_auc')
+    params = {'model_fitting__C': [0.001, 0.1, 1, 10]}
+    grid = GridSearchCV(pipe, params, cv=5, scoring='roc_auc')
     return grid
 
 
 def boosting_machine():
     """
-    lightgbm classifier grid search implementation
-    :return: grid search sklearn object
+    light gradient boosting machine classifier grid search implementation
+    :return: grid search sci-kit learn object
     :return:
     """
-    classifier = LGBMClassifier(n_jobs=-1, n_estimators=1e4, objective='binary', class_weights={0: 0.0001, 1: 1})
+    classifier = LGBMClassifier(n_jobs=-1, n_estimators=1000, objective='binary')
     pipe = basic_pipeline(classifier)
-    param_grid = {'model_fitting__max_depth': [-1, 3, 10, 30, 100, 300],
-                  'model_fitting__learning_rate ': [0.1, 1e-2, 1e-3, 1e-4],
-                  'model_fitting__reg_alpha': [0.1, 1e-2, 1e-3],
-                  'model_fitting__reg_lambda': [0.1, 1e-2, 1e-3]}
+    param_grid = {'model_fitting__learning_rate ': [0.1, 0.01],
+                  'model_fitting__reg_alpha': [0.1, 0.01, 0.001]}
     grid = GridSearchCV(pipe,
                         param_grid,
                         scoring=['roc_auc'],
-                        cv=20,
+                        cv=5,
                         refit='roc_auc')
     return grid
